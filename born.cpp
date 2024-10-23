455b6fb92012f3ece2d282602e29538a32aade25
VuongTrann101 <169137089+VuongTrann101@users.noreply.github.com>
2024-10-23 22:19:32 +0700

    updateborn

--- born.cpp
+++ born.cpp
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-//Ham xem co xuat hien phan tu x trong mang a khong
-int binary_search(int a[], int n, int x){
+
+//kieu struct chua nam sinh va so luong nguoi sinh trong nam
+typedef struct  {
+	int date, number;
+} Born;
+
+//Ham xem co xuat hien phan tu x trong mang a khong, tra ve chi so
+int binary_search(Born a[], int n, int x){
      int l = 0, r = n - 1;
     while(l<=r) {
        
         int m = (l + r) / 2;
-        if(a[m] == x){
-            return 1; } 
-        else if(a[m] < x){
+        if(a[m].number == x){
+            return m; } 
+        else if(a[m].number < x){
             
             l = m + 1;
         }
@@ -17,68 +23,77 @@ int binary_search(int a[], int n, int x){
             r = m - 1;
         }
     } 
-    return 0;
+    return -1;
 }
+
 
 //-------------------------------------------------------------------------------------------------------------------------
 
 
-// Ham dem so lan xuat hien phan tu x trong mang a
-int CountBS(int a[], int n, int x) {
-    int l = 0, r = n - 1, count = 0;
+// count[] chi so dem tuong ung vi tri cua moi nam sinh, neu nam sinh co so luong nguoi can tim thi tang bien dem
+
+// Ham tim tat ca vi tri xuat hien phan tu x trong mang a
+void CountBS(Born a[], int n, int x, int count[]) {
+    int l = 0, r = n - 1;
     while (l <= r) {
-        int m = (l + r) / 2;
+        int m= (l + r) / 2;
         
-        if (a[m] == x) {
-            
-            count++;
+        if (a[m].number == x) {
+            count[m]++;
+            ;
             
         //tim phia ben trai xem co su xuat hien cua x nua khong
             int left = m - 1;
-            while (left >= 0 && a[left] == x) {
-                count++;
+            while (left >= 0 && a[left].number == x) {
+                count[left]++;
                 left--;
             }
             
             //tim phia ben phai xem co su xuat hien cua x nua khong
             int right = m + 1;
-            while (right < n && a[right] == x) {
-                count++;
+            while (right < n && a[right].number == x) {
+                count[right]++;
                 right++;
             }
             
           break;
         }
-        else if (a[m] < x) {
+        else if (a[m].number < x) {
             l = m + 1;
         }
         else {
             r = m - 1;
         }
     }
-    return count;
+   
 }
 
 //-------------------------------------------------------------------------------------------------------------------------
 
-int FoundMax(int a[], int n)
- {
- 	for (int i = 0; i<n; i++) {
- 		if(a)
+
+int FoundMax(Born a[], int n)
+ { int index = 0;
+ 	for (int i = 1; i<n; i++) {
+ 		if(a[i].number>a[index].number) index=i;
 	 }
+	 return index;
  }
+ 
+int FoundMin(Born a[], int n)
+ { int index = 0;
+ 	for (int i = 1; i<n; i++) {
+ 		if(a[i].number < a[index].number) index = i;
+	 }
+	 return index;
+ }
+ 
+
 //-------------------------------------------------------------------------------------------------------------------------
 
-//kieu struct chua nam sinh va so luong nguoi sinh trong nam
-typedef struct  {
-	int date, number;
-} Born;
-
-//-------------------------------------------------------------------------------------------------------------------------
 
 int main() {
 
-     Born People[52] = {{1920, 19}, {1921, 77}, {1922, 40}, {1923, 90}, {1924, 2}, { 1925, 25}, {1926, 54},
+     Born People[51] = {{1920, 19}, {1921, 77}, {1922, 40}, {1923, 90}, {1924, 2}, { 1925, 25}, {1926, 54},
 	{1927, 17}, {1928, 79}, {1929, 6}, {1930, 44}, {1931,24}, {1932, 14}, {1933, 4}, {1934, 95}, {1935, 47}, {1936, 66}, 
 	{1937, 48}, {1938, 23}, {1939, 98}, {1940, 15}, {1941, 86}, {1942, 25}, {1943, 50}, {1944, 9}, {1945, 29}, {1946, 64}, {1947, 3},
 	{1948, 67}, {1949, 4}, {1950, 90}, {1951, 81}, {1952, 74}, {1953, 34}, {1954, 98}, {1955, 13}, {1956, 87}, {1957, 96}, {1958, 56},
@@ -88,59 +103,39 @@ int main() {
 	
 	//-------------------------------------------------------------------------------------------------------------------------
 	
-	int dem[52]={0};//mang dem co chi so tuong ung voi chi so cua mang dang xet
 	
-   for (int i = 0; i < 51;i++) {
-   	
-   	 for (int j = i+1; j < 52; j++) {
-   	 	
-   	 	if (People[i].number==People[j].number) {
-		     dem[i]++;//xuat hien phan tu number giong nhau thi tang gia tri bien dem tai phan tu i
-			                               }
-		}
-   }
-   
-   //-------------------------------------------------------------------------------------------------------------------------
-   //in ra nhung nam co cung so luong nguoi sinh trong list
-   int i;
-   printf("So nam sinh co so luong nguoi sinh giong nhau la: \n"); 
-   for (int i = 0; i < 52; i++) {
-	   int j = i+1;
-   	    if (dem[i]!=0) { 
-		   printf("So nam sinh co so luong nguoi sinh %d la: %d ", People[i].number, People[i].date); 
-   	    while(j<52) {
-		   
-   	if (People[i].number==People[j].number) {
-   		printf("%d ", People[j].date ); }
-   		j++;
-	   }  
-	   printf("\n");
-	   
-	   } }
-	   
-	// ---------------------------------------------------------------------------------------------------------------------------- 
+	int count[51]={0};
+    int x; 
+	printf("Nhap so can tim x la: "); scanf("%d", &x);
+	
+    CountBS(People,51,x,count);
+    //tim k thay x thi in ra not found
+    int i = 0;
+    while (i<51) { 
+	if(count[i]!=0) {
+    printf("So nam co cung so luong %d la: ",x); 
+    break; }
+    i++;
+    if(i==51) printf("Not Found");
+	 }
+    
+    for (int i = 0; i<51;i++) {
+    	if (count[i]!=0) printf("%d ", People[i].date);
+}
 
-	//tim trong mang nhung nam co so luong nguoi sinh theo yeu cau
 	
-	int x, dem[52] = 0; //reset bien dem 
-	printf("Nhap so can tim: "); scanf("%d", &x); // nhap so number can tim
-    	if(binary_search(People,52,x)) {
-    	printf("Found\n"); 
-    	
-   	//liet ke so nam co cung x can tim
-   	 for (int i = 0; i < 51;i++) {
-   	 	if (People[i].number==x) 
-   	 	
-		     dem[i]++;//xuat hien phan tu number giong nhau thi tang gia tri bien dem tai phan tu i
-		     
-			  if(dem[i]!=0) printf("%d ",People[i].date); }                        
-}        else printf("Not found/n");
+	//--------------------------------------------------------------------------------------------------------------------------
+  
 	
-	
-	//---------------------------------------------------------------------------------------------------------------------------
+	// tim max, min
    
-   // tim max, min 
-   int 
+   printf("\nNam nguoi sinh nhieu nhat va it nhat la: %d va %d \n", People[FoundMax(People,51)].date, People[FoundMin(People,51)].date);
    
+
    return 0;
 }
+
+
+
+	
+   
